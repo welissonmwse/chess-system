3365e7e2a5b5bd3361c889e3d43398015dcfcbdb
Silveira <welisson.silveira@seatecnologia.com.br>
2025-08-12 14:56:08 -0300

    Chess layer and printing the board

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import boardgame.Position;
-
-public class App {
-  public static void main(String[] args) throws Exception {
-    Position pos = new Position(3, 5);
-    System.out.println(pos);
-  }
-}
